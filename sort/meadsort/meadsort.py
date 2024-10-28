3a56f83331144594a3411f787f68d9b1ab733a46
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-10-27 21:31:54 -0600

    Fixed mead sort

--- sort/meadsort/meadsort.py
+++ sort/meadsort/meadsort.py
@@ -11,10 +11,10 @@ def meadsort(elements):
 
         return elements
 
-    m = sum(elements) / length
+    m_pivot = sum(elements) / length
 
     for v in elements:
-        if v > m:
+        if v > m_pivot:
             _max.append(v)
         else:
             _min.append(v)
@@ -23,3 +23,35 @@ def meadsort(elements):
     _max = meadsort(_max)
 
     return _min + _max
+
+
+def meadsort_f(elements):
+    """Mead sort (Fixed)."""
+    length = len(elements)
+    left, right = [], []
+
+    if length <= 1:
+        return elements
+    elif length == 2:
+        if elements[0] > elements[1]:
+            return [elements[1], elements[0]]
+
+        return elements
+
+    mead = sum(elements) / length
+    difference = elements[0]
+    difference_detected = False
+
+    for v in elements:
+        if v > mead:
+            right.append(v)
+        else:
+            left.append(v)
+
+        if not difference_detected and difference != v:
+            difference_detected = True
+
+    if not difference_detected:
+        return elements
+
+    return meadsort_f(left) + meadsort_f(right)
